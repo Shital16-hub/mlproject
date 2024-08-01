feac25051455fe5d87a4966954f8705ff9265c05
unknown <shitalnandre108@gmail.com.com>
2024-08-01 08:52:31 +0530

    Updated .gitignore to exclude conda environment

---
+++ setup.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return ist of requirements
+
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace("\n", " ") for req in requirements]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    outher="Shital",
+    author_email="shitalnandre108@gmail.com",
+    packages=find_packages(),
+    install_require=get_requirements("requirements.txt"),
+)
